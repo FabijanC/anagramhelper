58bc35ab1a6d138e59125aa6fbe50a540d160fe7
fabijancorak <fc49106@fer.hr>
2019-05-27 15:08:01 +0200

    added support for all latin based letters; modified the look

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isLetter(code) {
-    return code >= 65 && code <= 90 || code >= 97 || code <= 122;
+    let chr = String.fromCharCode(code);
+    return chr.toUpperCase() != chr.toLowerCase();
+    //return code >= 65 && code <= 90 || code >= 97 || code <= 122;
 }
 
-$("#up").on("keydown", e => {
-    let up = $("#up");
-    let down = $("#down");
+function keepChars(str) {
+    var ret = "";
+    for (var i = 0; i < str.length; ++i) {
+        if (isLetter(str.charCodeAt(i))) {
+            ret += str.charAt(i);
+        }
+    }
+    return ret;
+}
+
+$("#anagram").on("keydown", e => {
+    let anagram = $("#anagram");
+    let original = $("#original");
     let code = e.keyCode;
     
-    let ss = up[0].selectionStart, se = up[0].selectionEnd;
+    let ss = anagram[0].selectionStart, se = anagram[0].selectionEnd;
     
     if (code != 8 && code != 46) {
         return;
@@ -15,7 +27,7 @@ $("#up").on("keydown", e => {
     
     e.preventDefault();
     
-    let oldval = up.val(), deleted;
+    let oldval = anagram.val(), deleted;
     
     if (ss === se) {
         if (code === 8) {
@@ -23,70 +35,68 @@ $("#up").on("keydown", e => {
                 return;
             }
             deleted = oldval[ss-1];
-            up.val(oldval.substring(0, ss-1) + oldval.substring(ss));
-            up[0].selectionStart = up[0].selectionEnd = ss-1;
+            anagram.val(oldval.substring(0, ss-1) + oldval.substring(ss));
+            anagram[0].selectionStart = anagram[0].selectionEnd = ss-1;
         } else { // code == 46
-            if (se == up.val().length) {
+            if (se == anagram.val().length) {
                 return;
             }
             deleted = oldval[ss];
-            up.val(oldval.substring(0, ss) + oldval.substring(ss+1));
-            up[0].selectionStart = up[0].selectionEnd = ss;
+            anagram.val(oldval.substring(0, ss) + oldval.substring(ss+1));
+            anagram[0].selectionStart = anagram[0].selectionEnd = ss;
         }
     } else {
-        up.val(oldval.substring(0, ss) + oldval.substring(se));
+        anagram.val(oldval.substring(0, ss) + oldval.substring(se));
         deleted = oldval.substring(ss, se);
     }
-    down.val(down.val()+deleted.replace(/\W/, ""));
+    
+    original.val(original.val() + keepChars(deleted));
 });
 
-$("#up").on("keypress", e => {
+$("#anagram").on("keypress", e => {
     e.preventDefault();
-    let up = $("#up");
-    let down = $("#down");
+    let anagram = $("#anagram");
+    let original = $("#original");
     let code = e.keyCode;
 
-    if (code >= 97 && code <= 122) {
-        code -= 97-65;
-    }
+    code = String.fromCharCode(code).toUpperCase().charCodeAt(0);
     let pressed = String.fromCharCode(code);
     
-    let material = down.val();
+    let material = original.val();
     
-    if (code >= 65 && code <= 90) {                    
+    if (isLetter(code) && pressed === pressed.toUpperCase()) {                    
         if (material.indexOf(pressed) == -1) {
             return;
         }
-        down.val(material.replace(pressed, ""));
+        original.val(material.replace(pressed, ""));
     }
     
-    let oldval = up.val();
-    let newval = oldval.substring(0, up[0].selectionStart) + pressed + oldval.substring(up[0].selectionEnd);
-    let se = up[0].selectionEnd;
-    up.val(newval);
-    up[0].selectionStart = up[0].selectionEnd = se+1;
+    let oldval = anagram.val();
+    let newval = oldval.substring(0, anagram[0].selectionStart) + pressed + oldval.substring(anagram[0].selectionEnd);
+    let se = anagram[0].selectionEnd;
+    anagram.val(newval);
+    anagram[0].selectionStart = anagram[0].selectionEnd = se+1;
 });
 
-$("#down").on("keypress", e => {
+$("#original").on("keypress", e => {
     e.preventDefault();
-    let up = $("#up");
-    let down = $("#down");
+    let anagram = $("#anagram");
+    let original = $("#original");
     let code = e.keyCode;
-    if (code >= 97 && code <= 122) {
-        code -= 97-65;
-    }
-    let pressed = String.fromCharCode(code);
     
-    let oldval = down.val();
-    let newval = oldval.substring(0, down[0].selectionStart) + pressed + oldval.substring(down[0].selectionEnd);
-    let newStartSelection = down[0].selectionEnd+pressed.length;
-    down.val(newval);
-    down[0].selectionStart = newStartSelection;
-    down[0].selectionEnd = newStartSelection;
+    code = String.fromCharCode(code).toUpperCase().charCodeAt(0);
+    let pressed = String.fromCharCode(code).toUpperCase();
+    
+    let oldval = original.val();
+    let newval = oldval.substring(0, original[0].selectionStart) + pressed + oldval.substring(original[0].selectionEnd);
+    let newStartSelection = original[0].selectionEnd+pressed.length;
+    original.val(newval);
+    original[0].selectionStart = newStartSelection;
+    original[0].selectionEnd = newStartSelection;
 });
 
 $("#shuffle").on("click", e => {
-    var arr = $("#down").val().split("");
+    var arr = $("#original").val().split("");
     var curr = arr.length, tmp, randIndex;
     
     while (0 !== curr) {
@@ -98,5 +108,5 @@ $("#shuffle").on("click", e => {
         arr[rand] = tmp;
     }
     
-    $("#down").val(arr.join(""));
+    $("#original").val(arr.join(""));
 });
